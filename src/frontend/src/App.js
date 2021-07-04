df15d3efe7a4e75c740dec566d1f68a52fc771d6
A3Tom <Tom_Esq@Yahoo.com>
2021-07-04 18:39:07 +0100

    Ok now things work

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+     <div className="App">
+       <p>Remo</p>
+     </div>
+   );
 }
 
 export default App;
