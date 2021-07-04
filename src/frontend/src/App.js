96c8d12951cb4e155dc0b75748ad3e20165a81b1
A3Tom <Tom_Esq@Yahoo.com>
2021-07-04 21:34:16 +0100

    Added map components and call + parse of NASA API

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,28 @@
-import React, { Component }  from 'react';
-import './App.css';
+import React, { useState, useEffect }  from 'react';
+import Map from './components/Map';
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events')
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  }, [])
+
   return (
-     <div className="App">
-       <p>Remo</p>
-     </div>
+    <div>
+       { !loading ? <Map eventData={eventData} /> : <h1>Loading</h1>  }
+    </div>
    );
 }
 
